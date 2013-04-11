e220a32e9844336f5217a6845a1305b0722a05ea
Musa Hindi <musa.hindi@gmail.com>
2013-04-10 23:15:51 -0500

    Add initial implementation for command line options

--- vimeo_downloader.sh
+++ vimeo_downloader.sh
@@ -19,16 +19,45 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 
-if [ $# -ne 1 ]; then
-	echo "Vimeo Downloader v0.3.1"
-	echo "by Denver Gingerich (http://ossguy.com/)"
-	echo "    with script improvements by Jori Hamalainen"
-	echo "    updated for new Vimeo site by John Slade"
-	echo
-	echo "Usage: $0 <vimeo_id|vimeo_URL>"
+USAGE="Vimeo Downloader v0.3.1\n  by Denver Gingerich (http://ossguy.com/)\n  with script improvements by Jori Hamalainen\n  updated for new Vimeo site by John Slade\n\nUsage: $0 -[icsevh] arg"
+
+while getopts :i:c:s:e:hv OPT; do
+    case "$OPT" in
+        i)
+            VIMEO_ID=$OPTARG
+            echo "$VIMEO_ID"
+            ;;
+        c)
+        	CHANNEL_ID=$OPTARG 
+        	echo "$CHANNEL_ID"
+        	;;
+		s)
+			START_PAGE=$OPTARG
+			echo "$START_PAGE"	
+			;;
+		e)
+			END_PAGE=$OPTARG
+			echo "$END_PAGE"
+			;;
+        h)
+            echo $USAGE
+            exit 0
+            ;;
+        v)
+            echo "`basename $0` version 0.3.1"
+            exit 0
+            ;;
+        \?)
+            echo $USAGE >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [[ $# -eq 0 ]]; then
+	echo $USAGE
 	exit 1
 fi
-VIMEO_ID=`echo $1 | awk -F / '{print $NF}'`
 
 # Set the user agent ID to use
 USER_AGENT="Mozilla/5.0"
@@ -44,52 +73,61 @@ else
 		GET_CMD="curl -L -A ${USER_AGENT} "
 	else
 		echo "Could not find wget or curl"
-		exit 2
+		exit 2	
 	fi
 fi
 
 which perl
 if [ $? -eq 0 ]; then
 	echo "Using perl..."
-	USING_PERL=1
+	USING_PERL=1	
 else
 	echo "Using sed..."
 	USING_PERL=0
 fi
 
-VIDEO_XML=`${GET_CMD} http://vimeo.com/${VIMEO_ID}`
+download_video () {
 
-if [ $USING_PERL -eq 1 ]; then
-	REQUEST_SIGNATURE=`echo $VIDEO_XML | perl -e '@text_in = <STDIN>; if (join(" ", @text_in) =~ /"signature":"(.*?)"/i ){ print "$1\n"; }'`
-	REQUEST_SIGNATURE_EXPIRES=`echo $VIDEO_XML | perl -e '@text_in = <STDIN>; if (join(" ", @text_in) =~ /"timestamp":(\d*?),/i ){ print "$1\n"; }'`
-	CAPTION=`echo $VIDEO_XML | perl -p -e '/^.*?\<meta property="og:title" content="(.*?)"\>.*$/; $_=$1; s/[^\w.]/-/g;'`
-	ISHD=`echo $VIDEO_XML |    perl -p -e '/^.*?\<meta itemprop="videoQuality" content="(HD)"\>.*$/; $_=lc($1)||"sd";'`
+	VIDEO_XML=`${GET_CMD} http://vimeo.com/$1`
 
-	FILENAME="${CAPTION}-(${ISHD}-${VIMEO_ID}).flv"
-else
+	if [ $USING_PERL -eq 1 ]; then
+		REQUEST_SIGNATURE=`echo $VIDEO_XML | perl -e '@text_in = <STDIN>; if (join(" ", @text_in) =~ /"signature":"(.*?)"/i ){ print "$1\n"; }'`
+		REQUEST_SIGNATURE_EXPIRES=`echo $VIDEO_XML | perl -e '@text_in = <STDIN>; if (join(" ", @text_in) =~ /"timestamp":(\d*?),/i ){ print "$1\n"; }'`
+		CAPTION=`echo $VIDEO_XML | perl -p -e '/^.*?\<meta property="og:title" content="(.*?)"\>.*$/; $_=$1; s/[^\w.]/-/g;'`
+		ISHD=`echo $VIDEO_XML |    perl -p -e '/^.*?\<meta itemprop="videoQuality" content="(HD)"\>.*$/; $_=lc($1)||"sd";'`
 
-	# TODO update the sed code to work with the new site format
-	echo "This version requires perl - exiting"
-	exit 2
-	
-	REQUEST_SIGNATURE=`echo $VIDEO_XML | sed -e 's/^.*<request_signature>\([^<]*\)<.*$/\1/g'`
-	REQUEST_SIGNATURE_EXPIRES=`echo $VIDEO_XML | sed -e 's/^.*<request_signature_expires>\([^<]*\)<.*$/\1/g'`
-	ISHD="sd"
-	FILENAME=${VIMEO_ID}.flv
+		FILENAME="${CAPTION}-(${ISHD}-${VIMEO_ID}).flv"
+	else
+
+		# TODO update the sed code to work with the new site format
+		echo "This version requires perl - exiting"
+		exit 2
+		
+		REQUEST_SIGNATURE=`echo $VIDEO_XML | sed -e 's/^.*<request_signature>\([^<]*\)<.*$/\1/g'`
+		REQUEST_SIGNATURE_EXPIRES=`echo $VIDEO_XML | sed -e 's/^.*<request_signature_expires>\([^<]*\)<.*$/\1/g'`
+		ISHD="sd"
+		FILENAME=${VIMEO_ID}.flv
+	fi
+
+	echo
+	echo "Downloading video ${VIMEO_ID} to ${FILENAME}..."
+	echo "Request_signature=${REQUEST_SIGNATURE}"
+	echo "Request_signature_expires=${REQUEST_SIGNATURE_EXPIRES}"
+	echo "Quality=${QUALITY}"
+	echo 
+
+	EXEC_CMD="${GET_CMD} http://player.vimeo.com/play_redirect?clip_id=${VIMEO_ID}&sig=${REQUEST_SIGNATURE}&time=${REQUEST_SIGNATURE_EXPIRES}&quality=${ISHD}&codecs=H264,VP8,VP6&type=moogaloop_local&embed_location=" 
+	echo "Executing ${EXEC_CMD}"
+	${EXEC_CMD} > "${FILENAME}"
+
+	echo
+	echo "Video ${VIMEO_ID} saved to ${FILENAME}"
+	echo `file "${FILENAME}"`
+	echo
+}
+
+if [[ $VIMEO_ID ]]; then
+	download_video $VIMEO_ID
 fi
 
-echo
-echo "Downloading video ${VIMEO_ID} to ${FILENAME}..."
-echo "Request_signature=${REQUEST_SIGNATURE}"
-echo "Request_signature_expires=${REQUEST_SIGNATURE_EXPIRES}"
-echo "Quality=${QUALITY}"
-echo 
 
-EXEC_CMD="${GET_CMD} http://player.vimeo.com/play_redirect?clip_id=${VIMEO_ID}&sig=${REQUEST_SIGNATURE}&time=${REQUEST_SIGNATURE_EXPIRES}&quality=${ISHD}&codecs=H264,VP8,VP6&type=moogaloop_local&embed_location=" 
-echo "Executing ${EXEC_CMD}"
-${EXEC_CMD} > "${FILENAME}"
-
-echo
-echo "Video ${VIMEO_ID} saved to ${FILENAME}"
-echo `file "${FILENAME}"`
-echo
